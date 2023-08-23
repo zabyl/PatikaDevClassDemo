bdcc6b600ad9ea57e4cee1ae3749c6fc0458f426
UgrasK <ugurkaraer1989@gmail.com>
2023-08-23 06:03:12 +0300

    Add project files.

---
+++ ClassFieldProperty/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassFieldProperty
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Worker worker = new Worker();
+            worker.Name = "John Doe";
+            worker.Age = 25;
+            worker.Department = "Unknown";
+
+            worker.PrintWorker();
+
+            Console.ReadLine();
+        }
+    }
+
+    class Worker
+    {
+        public string Name;
+        public int Age;
+        public string Department;
+
+        public void PrintWorker()
+        {
+            Console.WriteLine($"Worker name: {Name}" +
+                $"\nAge: {Age}" +
+                $"\nDepartment: {Department}");
+        }
+    }
+}
